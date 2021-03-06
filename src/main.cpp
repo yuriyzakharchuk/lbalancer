9a047c4748de6c93aac110bb9500e2bf182a2952
Yuriy Zakharchuk <y.a.zakharchuk@gmail.com>
2021-03-06 22:56:29 +0200

    damon implementation added

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
-#include "server/server.hpp"
+#include "core/server/server.hpp"
 
-#include "helpers/configurator.hpp"
+//#include "helpers/configurator.hpp"
+#include "os/unix/unix.hpp"
 
 int
 main(int argc, char *argv[]) {
     // testing: listening all IPv4 addresses for connection on port 5000
-    // TODO: make daemon
     // TODO: parse settings file
+    // TODO: parse args
 
     // lb::helpers::configurator configurator;
+//#ifdef NDEBUG
+    if (lb::os_unix::daemonize()) {
+
+    }
+//#endif //NDEBUG
 
     lb::server server { "0.0.0.0", "5000", 6 };
     server.run();
--- src/os/unix/daemonize.cpp
+++ src/os/unix/daemonize.cpp
@@ -4,7 +4,66 @@
 
 #include "daemonize.hpp"
 
+extern "C" {
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+}
+
+#include <cstdlib>
+#include <iostream>
+
+namespace {
+    bool
+    already_opened() {
+        return false;
+    }
+}
+
+
 bool
 lb::os_unix::daemonize() {
-    return false;
+    umask(0); // return value: always succeeds
+    auto pid { fork() };
+    switch (pid) {
+        case -1: {
+            //TODO: failure handling
+            std::exit(1);
+        }
+        case 0: {
+            // in the child
+            break;
+        }
+        default: {
+            // in the parent
+#ifndef NDEBUG
+            std::cout << "Demonized successfully."
+                      << " Child's pid: " << pid
+                      << std::endl;
+#endif //NDEBUG
+            std::exit(0);
+        }
+    }
+    if(setsid() == -1) {
+        //TODO: failure handling
+    }
+
+    // stdin, stdout, stderr to /dev/null
+    auto null_fd { open("/dev/null", O_RDWR) };
+    if(null_fd == -1) {
+        return false;
+    }
+
+    if(dup2(null_fd, STDIN_FILENO) == -1) {
+        return false;
+    }
+    if(dup2(null_fd, STDOUT_FILENO) == -1) {
+        return false;
+    }
+    if(dup2(null_fd, STDERR_FILENO) == -1) {
+        return false;
+    }
+
+    return true;
 }
