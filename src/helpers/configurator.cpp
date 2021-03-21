387fb65372919c78182e389edf4c8c2e04b7a5e7
Yuriy Zakharchuk <y.a.zakharchuk@gmail.com>
2021-03-21 21:24:45 +0200

    added parsing of config file

--- src/helpers/configurator.cpp
+++ src/helpers/configurator.cpp
@@ -5,10 +5,14 @@
 #include <iostream>
 #include "configurator.hpp"
 
+using namespace lb::helpers;
 
-lb::helpers::configurator::configurator(int argc, char **argv) {
+configurator::configurator(int argc, char **argv) {
+    // TODO: parse args
+
     try {
         YAML::Node config = YAML::LoadFile(default_config_path);
+
         if(YAML::Node global = config["global"]) {
             parse_worker_count(global);
             parse_error_log(global);
@@ -18,8 +22,16 @@ lb::helpers::configurator::configurator(int argc, char **argv) {
         else {
             throw std::runtime_error("Config: global section not found");
         }
-        if(YAML::Node frontends = config["frontends"]) {
 
+        if(YAML::Node backends = config["backends"]) {
+            parse_backends(backends);
+        }
+        else {
+            throw std::runtime_error("Config: backends section not found");
+        }
+
+        if(YAML::Node frontends = config["frontends"]) {
+            parse_frontends(frontends);
         }
         else {
             throw std::runtime_error("Config: frontends section not found");
@@ -33,63 +45,58 @@ lb::helpers::configurator::configurator(int argc, char **argv) {
 
 
 unsigned int
-lb::helpers::configurator::worker_count() const noexcept {
+configurator::worker_count() const noexcept {
     return worker_count_;
 }
 
 
 bool
-lb::helpers::configurator::use_ipc() const noexcept {
+configurator::use_ipc() const noexcept {
     return ipc_;
 }
 
 
 bool
-lb::helpers::configurator::use_threads() const noexcept {
+configurator::use_threads() const noexcept {
     return !use_ipc();
 }
 
 
 std::string
-lb::helpers::configurator::error_log() const noexcept {
+configurator::error_log() const noexcept {
     return error_log_;
 }
 
 
 std::string
-lb::helpers::configurator::info_log() const noexcept {
+configurator::info_log() const noexcept {
     return info_log_;
 }
 
 
-lb::helpers::configurator::frontend_pool_t
-lb::helpers::configurator::construct(workers::service_pool& service_pool) {
-    for (int i = 0; i < 1; ++i) {
-        // TODO: refactor that shit
-        std::vector<std::pair<std::string, std::string>> servers;
-        servers.emplace_back(std::pair("127.0.0.1", "8000"));
+configurator::frontend_pool_t
+configurator::construct(workers::service_pool& service_pool) {
+    frontend_pool_t frontend_pool {};
+    frontend_pool.reserve(meta_frontend_pool_.size());
 
-        // TODO: session mode handling
-        auto f = lb::frontend::frontend{
-                "0.0.0.0",
-                "5000",
-                service_pool,
-                lb::backend::backend_pool{
-                        lb::backend::strategy::round_robin,
-                        servers,
-                        service_pool.get_service(),
-                        lb::session::mode::tcp
-                },
-                lb::session::mode::tcp
-        };
-        frontend_pool_.emplace_back(std::move(f));
+    for(auto& meta_frontend : meta_frontend_pool_) {
+        // creating backend pool for current meta frontend
+        auto it = std::find_if(meta_backend_pool_.begin(), meta_backend_pool_.end(),
+                               [&meta_frontend](const meta_backend& m) { return m.name == meta_frontend.backend; });
+        if(it == meta_backend_pool_.end()) {
+            std::exit(1);
+        }
+        backend::backend_pool backend_pool { *it };
+
+        // creating frontend
+        frontend_pool.emplace_back(meta_frontend, service_pool, backend_pool);
     }
-    return std::move(frontend_pool_);
+    return frontend_pool;
 }
 
 
 void
-lb::helpers::configurator::parse_worker_count(YAML::Node& global) {
+configurator::parse_worker_count(YAML::Node& global) {
     if(auto node = global["worker_process"]) {
         auto workers = node.as<std::string>();
         if (workers == "auto") {
@@ -117,7 +124,7 @@ lb::helpers::configurator::parse_worker_count(YAML::Node& global) {
 
 
 void
-lb::helpers::configurator::parse_error_log(YAML::Node& global) {
+configurator::parse_error_log(YAML::Node& global) {
     if(auto node = global["error_log"]) {
         auto path = node.as<std::string>();
         // TODO: validate path
@@ -131,7 +138,7 @@ lb::helpers::configurator::parse_error_log(YAML::Node& global) {
 
 
 void
-lb::helpers::configurator::parse_info_log(YAML::Node& global) {
+configurator::parse_info_log(YAML::Node& global) {
     if(auto node = global["info_log"]) {
         auto path = node.as<std::string>();
         // TODO: validate path
@@ -145,12 +152,185 @@ lb::helpers::configurator::parse_info_log(YAML::Node& global) {
 
 
 void
-lb::helpers::configurator::parse_use_ipc(YAML::Node& global) {
-    if (auto value = global["use_ipc"]) {
+configurator::parse_use_ipc(YAML::Node& global) {
+    if (global["use_ipc"]) {
         ipc_ = global["use_ipc"].as<bool>();
     }
     else {
         ipc_ = false;
+    }
+}
+
+
+void
+configurator::parse_backends(YAML::Node& node) {
+    for(YAML::iterator it = node.begin(); it != node.end(); ++it) {
+        auto key { it->first };
+        auto value { it->second };
+        meta_backend_pool_.emplace_back(meta_backend {
+                key.as<std::string>(),
+                parse_balance(value),
+                parse_mode(value),
+                parse_servers(value)
+        });
+    }
+}
+
+
+void
+configurator::parse_frontends(YAML::Node& frontends) {
+    for(YAML::iterator it = frontends.begin(); it != frontends.end(); ++it) {
+        meta_frontend_pool_.emplace_back(meta_frontend {
+                it->first.as<std::string>(),
+                parse_address(it->second),
+                parse_port(it->second),
+                parse_backend(it->second),
+                parse_mode(it->second)
+        });
+    }
+}
+
+
+std::string
+configurator::parse_port(YAML::Node& node) {
+    try{
+        if (auto value = node["port"]) {
+            return value.as<std::string>();
+        }
+        else {
+            return "0";
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        std::exit(1);
+    }
+}
+
+
+std::string
+configurator::parse_address(YAML::Node& node) {
+    try{
+        if (auto value = node["address"]) {
+            return value.as<std::string>();
+        }
+        else {
+            return "";
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        std::exit(1);
+    }
+}
+
+
+lb::session::mode
+configurator::parse_mode(YAML::Node &node) {
+    try{
+        if (auto value = node["mode"]) {
+            auto mode = value.as<std::string>();
+            if (mode == "http") {
+                return lb::session::mode::http;
+            }
+            else if (mode == "ssl") {
+                return lb::session::mode::ssl;
+            }
+            else {
+                return lb::session::mode::tcp;
+            }
+        }
+        else {
+            return lb::session::mode::tcp;
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        std::exit(1);
+    }
+}
+
+
+lb::backend::strategy
+configurator::parse_balance(YAML::Node& node) {
+    try {
+        if (auto value = node["balance"]) {
+            auto mode = value.as<std::string>();
+            return lb::backend::strategy::round_robin;
+        }
+        else {
+            return lb::backend::strategy::round_robin;
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        std::exit(1);
+    }
+}
+
+
+std::string
+configurator::parse_backend(YAML::Node& node) {
+    try{
+        if (auto value = node["backend"]) {
+            return value.as<std::string>();
+        }
+        else {
+            throw std::runtime_error("Config: can't parse backend value");
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        std::exit(1);
+    }
+}
+
+
+meta_server_pool
+configurator::parse_servers(YAML::Node& node) {
+    meta_server_pool servers {};
+    servers.reserve(node.size());
+
+    auto servers_map = node["servers"];
+    for(YAML::iterator it = servers_map.begin(); it != servers_map.end(); ++it) {
+        auto key { it->first };
+        auto value { it->second };
+        servers.emplace_back(meta_server {
+                key.as<std::string>(),
+                parse_address(value),
+                parse_port(value),
+                parse_weight(value),
+                parse_is_backup(value)
+        });
+    }
+    return servers;
+}
+
+
+unsigned int
+configurator::parse_weight(YAML::Node& node) {
+    try{
+        if (auto value = node["weight"]) {
+            return value.as<unsigned int>();
+        }
+        else {
+            return 0;
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        std::exit(1);
+    }
+}
+
+
+bool
+configurator::parse_is_backup(YAML::Node& node) {
+    if (node["is_backup"]) {
+        return node["is_backup"].as<bool>();
+    }
+    else {
+        return false;
     }
 }
 
--- src/helpers/configurator.hpp
+++ src/helpers/configurator.hpp
@@ -8,8 +8,14 @@
 #include <string>
 #include <vector>
 #include <yaml-cpp/yaml.h>
+#include "../core/backend/strategy.hpp"
 #include "../core/frontend/frontend.hpp"
+#include "../core/backend/backend_pool.hpp"
 #include "../core/workers/service_pool.hpp"
+
+#include "meta/meta_server.hpp"
+#include "meta/meta_backend.hpp"
+#include "meta/meta_frontend.hpp"
 
 
 namespace lb::helpers {
@@ -24,7 +30,6 @@ namespace lb::helpers {
         configurator& operator=(configurator&&) = delete;
         configurator& operator=(const configurator&) = delete;
 
-
         static inline auto
         cpu_count() noexcept {
             return std::thread::hardware_concurrency();
@@ -38,28 +43,17 @@ namespace lb::helpers {
         static constexpr char* default_config_path = "c:/lbalancer/lbalancer.conf";
 #endif
 
-        frontend_pool_t
-        construct(workers::service_pool& service_pool);
+        frontend_pool_t construct(workers::service_pool& service_pool);
 
+        [[nodiscard]] unsigned int worker_count() const noexcept;
 
-        [[nodiscard]] unsigned int
-        worker_count() const noexcept;
+        [[nodiscard]] inline bool use_ipc() const noexcept;
 
+        [[nodiscard]] inline bool use_threads() const noexcept;
 
-        [[nodiscard]] inline bool
-        use_ipc() const noexcept;
+        [[nodiscard]] inline std::string error_log() const noexcept;
 
-
-        [[nodiscard]] inline bool
-        use_threads() const noexcept;
-
-
-        [[nodiscard]] inline std::string
-        error_log() const noexcept;
-
-
-        [[nodiscard]] inline std::string
-        info_log() const noexcept;
+        [[nodiscard]] inline std::string info_log() const noexcept;
 
 
     private:
@@ -68,20 +62,30 @@ namespace lb::helpers {
         std::string  info_log_ {};
         bool         ipc_ {};
 
-        frontend_pool_t frontend_pool_;
+        meta_frontend_pool meta_frontend_pool_;
+        meta_backend_pool  meta_backend_pool_;
 
         // global
-        void
-        parse_worker_count(YAML::Node&);
+        void parse_worker_count(YAML::Node&);
+        void parse_error_log(YAML::Node&);
+        void parse_info_log(YAML::Node&);
+        void parse_use_ipc(YAML::Node&);
 
-        void
-        parse_error_log(YAML::Node&);
+        // frontends section
+        void parse_frontends(YAML::Node&);
 
-        void
-        parse_info_log(YAML::Node&);
+        // backends section
+        void parse_backends(YAML::Node&);
+        static meta_server_pool parse_servers(YAML::Node&);
 
-        void
-        parse_use_ipc(YAML::Node&);
+        // helpers
+        static unsigned int parse_weight(YAML::Node&);
+        static bool parse_is_backup(YAML::Node&);
+        static std::string parse_port(YAML::Node&);
+        static std::string parse_address(YAML::Node&);
+        static session::mode parse_mode(YAML::Node&);
+        static backend::strategy parse_balance(YAML::Node&);
+        static std::string parse_backend(YAML::Node&);
     };
 }
 
