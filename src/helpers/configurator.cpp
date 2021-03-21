4c0c2b5f125a90cde622d1f2385c4cc070804ff2
Yuriy Zakharchuk <y.a.zakharchuk@gmail.com>
2021-03-21 14:38:27 +0200

    configurator added

--- src/helpers/configurator.cpp
+++ src/helpers/configurator.cpp
@@ -2,14 +2,155 @@
 // Created by yuriy on 2/10/21.
 //
 
+#include <iostream>
 #include "configurator.hpp"
-#include <libconfig.h++>
 
-lb::helpers::configurator::configurator() {
 
+lb::helpers::configurator::configurator(int argc, char **argv) {
+    try {
+        YAML::Node config = YAML::LoadFile(default_config_path);
+        if(YAML::Node global = config["global"]) {
+            parse_worker_count(global);
+            parse_error_log(global);
+            parse_info_log(global);
+            parse_use_ipc(global);
+        }
+        else {
+            throw std::runtime_error("Config: global section not found");
+        }
+        if(YAML::Node frontends = config["frontends"]) {
+
+        }
+        else {
+            throw std::runtime_error("Config: frontends section not found");
+        }
+    }
+    catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        std::exit(1);
+    }
 }
 
-int
-lb::helpers::configurator::thread_count() {
-    return 0;
+
+unsigned int
+lb::helpers::configurator::worker_count() const noexcept {
+    return worker_count_;
 }
+
+
+bool
+lb::helpers::configurator::use_ipc() const noexcept {
+    return ipc_;
+}
+
+
+bool
+lb::helpers::configurator::use_threads() const noexcept {
+    return !use_ipc();
+}
+
+
+std::string
+lb::helpers::configurator::error_log() const noexcept {
+    return error_log_;
+}
+
+
+std::string
+lb::helpers::configurator::info_log() const noexcept {
+    return info_log_;
+}
+
+
+lb::helpers::configurator::frontend_pool_t
+lb::helpers::configurator::construct(workers::service_pool& service_pool) {
+    for (int i = 0; i < 1; ++i) {
+        // TODO: refactor that shit
+        std::vector<std::pair<std::string, std::string>> servers;
+        servers.emplace_back(std::pair("127.0.0.1", "8000"));
+
+        // TODO: session mode handling
+        auto f = lb::frontend::frontend{
+                "0.0.0.0",
+                "5000",
+                service_pool,
+                lb::backend::backend_pool{
+                        lb::backend::strategy::round_robin,
+                        servers,
+                        service_pool.get_service(),
+                        lb::session::mode::tcp
+                },
+                lb::session::mode::tcp
+        };
+        frontend_pool_.emplace_back(std::move(f));
+    }
+    return std::move(frontend_pool_);
+}
+
+
+void
+lb::helpers::configurator::parse_worker_count(YAML::Node& global) {
+    if(auto node = global["worker_process"]) {
+        auto workers = node.as<std::string>();
+        if (workers == "auto") {
+            worker_count_ = cpu_count();
+        }
+        else {
+            try {
+                worker_count_ = std::stoi(workers);
+            }
+            catch(const std::invalid_argument& e) {
+                std::cerr << e.what() << std::endl;
+                worker_count_ = cpu_count();
+            }
+            catch (const std::exception& e) {
+                std::cerr << e.what() << std::endl;
+                std::exit(1);
+            }
+        }
+    }
+    else {
+        std::cout << "Config: no worker_process parameter found." << std::endl;
+        std::exit(1);
+    }
+}
+
+
+void
+lb::helpers::configurator::parse_error_log(YAML::Node& global) {
+    if(auto node = global["error_log"]) {
+        auto path = node.as<std::string>();
+        // TODO: validate path
+        error_log_ = path;
+    }
+    else {
+        // TODO: default path of log OR off error logging
+        error_log_.clear();
+    }
+}
+
+
+void
+lb::helpers::configurator::parse_info_log(YAML::Node& global) {
+    if(auto node = global["info_log"]) {
+        auto path = node.as<std::string>();
+        // TODO: validate path
+        info_log_ = path;
+    }
+    else {
+        // TODO: default path of log OR off error logging
+        info_log_.clear();
+    }
+}
+
+
+void
+lb::helpers::configurator::parse_use_ipc(YAML::Node& global) {
+    if (auto value = global["use_ipc"]) {
+        ipc_ = global["use_ipc"].as<bool>();
+    }
+    else {
+        ipc_ = false;
+    }
+}
+
--- src/helpers/configurator.hpp
+++ src/helpers/configurator.hpp
@@ -6,17 +6,82 @@
 #define LOAD_BALANCER_CONFIGURATOR_HPP
 
 #include <string>
+#include <vector>
+#include <yaml-cpp/yaml.h>
+#include "../core/frontend/frontend.hpp"
+#include "../core/workers/service_pool.hpp"
+
 
 namespace lb::helpers {
-    class configurator {
+    class configurator final {
     public:
-        configurator();
+        using frontend_pool_t = std::vector<lb::frontend::frontend>;
 
-        int thread_count();
+        configurator(int argc, char **argv);
+
+        configurator(configurator&&) = delete;
+        configurator(const configurator&) = delete;
+        configurator& operator=(configurator&&) = delete;
+        configurator& operator=(const configurator&) = delete;
+
+
+        static inline auto
+        cpu_count() noexcept {
+            return std::thread::hardware_concurrency();
+        }
+
+
+#if defined(__linux__)
+        static constexpr const char* default_config_path = "/etc/lbalancer/lbalancer.conf";
+#elif defined(_WIN64)
+        // TODO: where store windows config
+        static constexpr char* default_config_path = "c:/lbalancer/lbalancer.conf";
+#endif
+
+        frontend_pool_t
+        construct(workers::service_pool& service_pool);
+
+
+        [[nodiscard]] unsigned int
+        worker_count() const noexcept;
+
+
+        [[nodiscard]] inline bool
+        use_ipc() const noexcept;
+
+
+        [[nodiscard]] inline bool
+        use_threads() const noexcept;
+
+
+        [[nodiscard]] inline std::string
+        error_log() const noexcept;
+
+
+        [[nodiscard]] inline std::string
+        info_log() const noexcept;
+
 
     private:
-        static const std::string default_config_path_;
+        unsigned int worker_count_ {};
+        std::string  error_log_ {};
+        std::string  info_log_ {};
+        bool         ipc_ {};
 
+        frontend_pool_t frontend_pool_;
+
+        // global
+        void
+        parse_worker_count(YAML::Node&);
+
+        void
+        parse_error_log(YAML::Node&);
+
+        void
+        parse_info_log(YAML::Node&);
+
+        void
+        parse_use_ipc(YAML::Node&);
     };
 }
 
