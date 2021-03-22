a06399cfaeec6cc635c52d118feb00baddd0ee60
Yuriy Zakharchuk <y.a.zakharchuk@gmail.com>
2021-03-22 20:38:37 +0200

    make server class header only

--- src/core/server/server.hpp
+++ src/core/server/server.hpp
@@ -11,14 +11,24 @@
 namespace lb {
     class server {
     public:
-        explicit server(helpers::configurator& configurator);
-
         server(server&&) = delete;
         server(const server&) = delete;
         server& operator=(server&&) = delete;
         server& operator=(const server&) = delete;
 
-        void run();
+        explicit server(helpers::configurator& configurator)
+            : service_pool_(configurator.worker_count()),
+            signals_(service_pool_.service()) {
+            for(const auto& binding : configurator.construct()) {
+                std::make_shared<frontend::frontend>(binding.first,
+                                                     service_pool_.service(),
+                                                     std::move(backend::backend_pool(binding.second)))->start_accept();
+            }
+        }
+
+        void run() {
+            service_pool_.run_all();
+        }
 
     private:
         workers::service_pool service_pool_;
